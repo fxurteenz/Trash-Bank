36658652bed401a696130a4285d87730b350c5bd
Piakpound <pattarasawan_2542@hotmail.com>
2025-12-12 20:58:33 +0700

    Update : operater page & api
    - /operater page
    - /api/operater api
    - update auth

---
+++ App/public/views/operater/home.php
@@ -0,0 +1,32 @@
+<div class="grid grid-cols-1 lg:grid-cols-3 gap-4">
+    <div class="bg-white rounded-lg space-y-2 shadow md:mx-auto w-[100%] md:w-[50%] lg:w-[100%] lg:mx-0 px-4 py-4">
+        <div class="flex">
+            <h2 class="text-xl font-bold">ฝากขยะ</h2>
+        </div>
+        <div class="space-y-2 text-sm">
+            <div class="w-full flex justify-between">
+                <label class="w-2-6" for="">ชื่อผู้ฝาก</label>
+                <input class="w-4/6 px-1 py-1 border border-1 border-gray-300 shadow-xs rounded" type="text" name=""
+                    id="">
+            </div>
+            <div class="w-full flex justify-between">
+                <label class="w-2-6" for="">ประเภท</label>
+                <input class="w-4/6 px-1 py-1 border border-1 border-gray-300 shadow-xs rounded" type="text" name=""
+                    id="">
+            </div>
+            <div class="w-full flex justify-between">
+                <label class="w-2-6" for="">น้ำหนัก</label>
+                <input class="w-4/6 px-1 py-1 border border-1 border-gray-300 shadow-xs rounded" type="text" name=""
+                    id="">
+            </div>
+        </div>
+    </div>
+
+    <div class="bg-white lg:col-span-2 rounded-lg shadow p-6">
+        <div class="flex">
+            <h2 class="text-xl font-bold">ประวัติการดำเนินการ</h2>
+
+        </div>
+        
+    </div>
+</div>
