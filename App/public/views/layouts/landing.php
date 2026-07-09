ce61c2bc56f0ed79df02e8539307c54c99543947
Piakpound <pattarasawan_2542@hotmail.com>
2026-07-09 12:34:05 +0700

    mod : layout

--- App/public/views/layouts/landing.php
+++ App/public/views/layouts/landing.php
@@ -10,6 +10,8 @@
     <!-- <link href="/assets/output.css" rel="stylesheet"> -->
     <script defer src="/js/alpine-collapse.min.js"></script>
     <script defer src="/js/alpine.min.js"></script>
+    <script src="/js/chart.js"></script>
+
     <script type="text/javascript" src="js/lucide.min.js"></script>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@100..900&family=Noto+Serif+Thai:wght@100..900&family=Sarabun:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap');
